7010b7fe0a71479adf2998fcdace9a21016a809e
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-08-30 17:56:08 -0700

    added delete function

---
+++ routes/apiRoutes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+
+const fs = require('fs');
+
+const uniqid = require('uniqid');
+
+
+    router.get('/api/notes', (req, res) => {
+      let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+        res.json(notes);
+    });
+
+
+
+    router.post('/api/notes', (req, res) => {
+        const { title, text } = req.body;
+        console.log(uniqid()); // -> 4n5pxq24kpiob12og9
+        if (req.body) {
+            let oldNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+            const newNote = {
+                title,
+                text,
+                id: uniqid()
+                
+            };
+            console.log(oldNotes);
+            oldNotes.push(newNote)
+          let note = fs.writeFileSync('./db/db.json', JSON.stringify(oldNotes));
+        //   if (!note) {
+        //     res.json({ message: 'something went wrong.'})
+        //   }
+            res.json({ message: 'added new note.'})
+            
+        }
+        console.log(req.body);
+    });
+
+    router.delete('/api/notes/:id', (req, res) => {
+        const routerID = req.params.id;
+        let oldNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+        const result = oldNotes.filter((note) => note.id !== routerID);
+        note = fs.writeFileSync('./db/db.json', JSON.stringify(result));
+        res.json('note has been deleted.')
+    })
+
+    
+
+    module.exports = router;
--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-const api = require('./routes/index.js');
 // boiler plate
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(clog);
-
+const routes = require('./routes/apiRoutes.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
 app.use(express.static('public'));
 // GET route for landing page
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/assests/index.html'))
-);
+app.use(routes);
+
 // GET route for notes
 app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/assests/notes.html'))
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 // listen for server request from local host
 app.listen(PORT, () => 
